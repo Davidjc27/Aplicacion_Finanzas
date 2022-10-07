143ed3146e66fe331deed329daff83d98d2fd8a4
Davidjc27 <115188179+Davidjc27@users.noreply.github.com>
2022-10-07 09:23:36 -0500

    Se agregaron columnas Unique y Not null

--- Base_de_Datos.sql
+++ Base_de_Datos.sql
@@ -1,12 +1,12 @@
-create table usuarios(id int primary key,contraseña varchar(50),correoUsuario varchar(50),nombre varchar(50))
+create table usuarios(id int primary key,contraseña varchar(50) unique not null,correoUsuario varchar(50) unique not null,nombre varchar(50))
 
-Create table movimientos (idUsuario int,id_mov int primary key,tipo int,categoria int not null,cantidad int not null,
-							descripcion varchar(50),fecha date,
+Create table movimientos (idUsuario int,id_mov int primary key,tipo int not null,categoria int not null,cantidad int not null,
+							descripcion varchar(50),fecha date not null,
 							foreign key (tipo) references tipoMovimiento(id),
 							foreign key (categoria) references categoria(id),
 							foreign key (idUsuario) references usuarios(id))
 
-Create table categoria(id int primary key,descripcion varchar(30),tipo int
+Create table categoria(id int primary key,descripcion varchar(30) not null,tipo int
 						foreign key (tipo) references tipoMovimiento(id))
 
-create table tipoMovimiento(id int primary key,tipo varchar (15))
+create table tipoMovimiento(id int primary key,tipo varchar (15) not null)
